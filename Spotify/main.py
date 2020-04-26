9941567f3f0f1b0fb5aab2f242876b92d8e93222
Khac Minh Le <khac.minh.le@outlook.de>
2020-04-26 18:29:57 +0200

    update
    update

--- Spotify/main.py
+++ Spotify/main.py
@@ -3,9 +3,9 @@ from dbconnector import DatabaseHandler
 from extractdata import DataExtracter
 
 #Define the information needed to extract the data
-client_id = ""
+client_id = "1bec01f05d1149af8db0c2a85bc47121"
 scope = "user-read-recently-played"
-client_secret = ""
+client_secret = "f242ec154e1a47d99c3c1c8dc0e972aa"
 redirect_uri = "http://localhost:8888"
 
 #Check for Authorisation and get a token to make the requests in the spotify webapi
@@ -21,9 +21,9 @@ data = da.extract_data()
 
 #Write the data into a dataframe
 db = DatabaseHandler(host = "127.0.0.1",
-                     database = "",
-                     user = "",
-                     password = "",
+                     database = "spotify",
+                     user = "Minh",
+                     password = "gelberdelfin123",
                      auth_plugin = "mysql_native_password")
 
 db.connect()
