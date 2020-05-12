441f83904f97706ab5329f5fb7c79702845105d9
Khac Minh Le <khac.minh.le@outlook.de>
2020-05-12 21:31:21 +0200

    Updates

--- COVID19/Data_Processing.bat
+++ COVID19/Data_Processing.bat
@@ -1,6 +1,6 @@
 cd C:/Users/KhacM/GitHub/MinhLe/COVID19/JohnHopkinsData
-git merge origin/master --allow-unrelated-histories
-git pull https://github.com/CSSEGISandData/COVID-19
+
+git pull https://github.com/CSSEGISandData/COVID-19 
 
 cd C:/Users/KhacM/GitHub/MinhLe/COVID19
 py "C:/Users/KhacM/GitHub/COVID-19/DataProcessing.py"
